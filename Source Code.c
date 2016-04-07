7e41b17132b780cb580587fbf2960fe4aad7da57
Nishad Mandlik <Nishad Mandlik>
2016-04-08 01:52:44 +0530

    File Absent Error Solved

--- Source Code.c
+++ Source Code.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 
-typedef struct pix
+FILE *steg;
+
+struct pix
 {
 	unsigned char B;
 	unsigned char G;
@@ -10,12 +12,13 @@ typedef struct pix
 struct bmp
 {
 	int height;
-	int width
+	int width;
+	int padding;
 }bitmap;
 
-char conv_pix2char(pixel ip)
+char conv_pix2char()
 {
-		char op = (((ip.B)%5)*25 + ((ip.G)%5)*5 + ((ip.R)%5));
+		char op = (((pixel.B)%5)*25 + ((pixel.G)%5)*5 + ((pixel.R)%5));
 		if (op==0)
 		{
 			op=125;
@@ -23,180 +26,51 @@ char conv_pix2char(pixel ip)
 		return op;
 }
 
-pixel conv_char2pix(char ip, pixel op)
+void conv_char2pix(char ip)
 {
-	if (op.B==255)
+	if (pixel.B==255)
 	{
-		op.B=250;
+		pixel.B=250;
 	}
-	if (op.G==255)
+	if (pixel.G==255)
 	{
-		op.G=250;
+		pixel.G=250;
 	}
-	if (op.R==255)
+	if (pixel.R==255)
 	{
-		op.R=250;
+		pixel.R=250;
 	}
-	op.B=((op.B)/5)*5 + ((ip/25)%5);
-	op.G=((op.G)/5)*5 + ((ip/5)%5);
-	op.R=((op.R)/5)*5 + (ip%5);
-	return op; 
-}
-
-void read_data(FILE *img)
-{
-	fseek(img, 54, SEEK_SET);
-	printf("\n--Read Mode--\n");
-	pixel map;
-	int i, j, padding=0;
-	char ch=0;
-	if ((bitmap.width)%4!=0)
-	{
-		padding=4-((3*(bitmap.width))%4);
-	}
-	fread(&map, sizeof(pixel), 1, img);
-	if (conv_pix2char(map)!=2)
-	{
-		printf("No Existing Data Found !\n\n");
-		return;
-	}
-	for(i=0;i<bitmap.height;i++)
-	{
-		for(j=0;j<bitmap.width;j++)
-		{
-			fread(&map, sizeof(pixel), 1, img);
-			ch=conv_pix2char(map);
-			if (ch==10)
-			{
-				printf("\n--End Of Message--\n\n");
-				fclose(img);
-				return;
-			}
-			printf("%c", ch);
-		}
-		fseek(img, padding, SEEK_CUR);
-	}	
-}
-
-void write_data(FILE *img)
-{
-	fseek(img, 54, SEEK_SET);
-	printf("\n--Write Mode--\nEnter 0 To OverWrite Message\nEnter 1 To Add Text To The Existing Message\n");
-	pixel map;
-	int i, j, padding=0;
-	char wr_opt, ch=0;
-	if ((bitmap.width)%4!=0)
-	{
-		padding=4-((3*(bitmap.width))%4);
-	}
-	scanf("%c", &wr_opt);
-	getchar();
-	if (wr_opt==48)
-	{
-		fread(&map, sizeof(pixel), 1, img);
-		fseek(img, -sizeof(pixel), SEEK_CUR);
-		map=conv_char2pix(2, map);
-		fwrite(&map, sizeof(pixel), 1, img);		
-	}
-	else if (wr_opt==49)
-	{
-		fread(&map, sizeof(pixel), 1, img);
-		if (conv_pix2char(map)==2)
-		{
-			for(i=0;i<bitmap.height;i++)
-			{
-				for(j=0;j<bitmap.width;j++)
-				{
-					fread(&map, sizeof(pixel), 1, img);
-					if (conv_pix2char(map)==10)
-					{
-						fseek(img, -sizeof(pixel), SEEK_CUR);
-						break;
-					}
-				}
-				if (j<bitmap.width)
-				{
-					break;
-				}
-				fseek(img, padding, SEEK_CUR);					
-			}
-		}
-		else
-		{
-			printf("No Existing Data Found. Please Start From Scratch.\n");
-			fseek(img, -sizeof(pixel), SEEK_CUR);
-			map=conv_char2pix(2, map);
-			fwrite(&map, sizeof(pixel), 1, img);
-		}	
-	}
-	else
-	{
-		printf("Invalid Input !\n\n");
-		return;
-	}
-	fseek(img, sizeof(pixel), SEEK_CUR);
-	i=0;	
-	do
-	{
-		fseek(img, -sizeof(pixel), SEEK_CUR);
-		ch=getchar();
-		fread(&map, sizeof(pixel), 1, img);
-		map=conv_char2pix(ch, map);
-		fseek(img, -sizeof(pixel), SEEK_CUR);
-		fwrite(&map, sizeof(pixel), 1, img);
-		i+=1;
-		if (i==bitmap.width)
-		{
-			fseek(img, padding, SEEK_CUR);
-			i=0;
-		}
-		fseek(img, sizeof(pixel), SEEK_CUR);
-	}
-	while(ch!=10);
-	printf("\n--Message Recorded--\n\n");
-	fclose(img);
+	pixel.B=((pixel.B)/5)*5 + ((ip/25)%5);
+	pixel.G=((pixel.G)/5)*5 + ((ip/5)%5);
+	pixel.R=((pixel.R)/5)*5 + (ip%5);
 	return;
 }
 
-void reset_data(FILE *img)
-{
-	fseek(img, 54, SEEK_SET);
-	printf("\n--Reset Mode--\nPlease Wait");
-	int i,j, padding;
-	pixel map;
-	if ((bitmap.width)%4!=0)
-	{
-		padding=4-((3*(bitmap.width))%4);
-	}
-	fseek(img, sizeof(pixel), SEEK_CUR);
-	for(i=0;i<bitmap.height;i++)
-	{
-		for(j=0;j<bitmap.width;j++)
-		{
-			fseek(img, -sizeof(pixel), SEEK_CUR);
-			fread(&map, sizeof(pixel), 1, img);
-			map=conv_char2pix(3, map);
-			fseek(img, -sizeof(pixel), SEEK_CUR);
-			fwrite(&map, sizeof(pixel), 1, img);
-			fseek(img, sizeof(pixel), SEEK_CUR);	
-		}
-		fseek(img, padding, SEEK_CUR);
-		if (i%8==0)
-		{
-			printf(".");
-		}
-	}
-	fclose(img);
-	printf("\n--Bitmap Reset Successful--\n\n");
-	return;
-}
-
-void intrpt_header(FILE *img)
+void intrpt_header(FILE *steg)
 {
 	unsigned char head[54];
-	fread(head, sizeof(unsigned char), 54, img);
+	fread(head, sizeof(unsigned char), 54, steg);
 	bitmap.height=(int)head[18] + (int)head[19]*256 + (int)head[20]*256*256 + (int)head[21]*256*256*256;
 	bitmap.width=(int)head[22] + (int)head[23]*256 + (int)head[24]*256*256 + (int)head[25]*256*256*256;
+	if ((bitmap.width)%4!=0)
+	{
+		bitmap.padding=4-((3*(bitmap.width))%4);
+	}
+	return;
+}
+
+char read_char()
+{
+	fread(&pixel, sizeof(pixel), 1, steg);
+	return conv_pix2char();
+}
+
+void write_pix(char ch)
+{
+	fread(&pixel, sizeof(pixel), 1, steg);
+	conv_char2pix(ch);
+	fseek(steg, -sizeof(pixel), SEEK_CUR);
+	fwrite(&pixel, sizeof(pixel), 1, steg);
 	return;
 }
 
@@ -211,24 +85,125 @@ int main()
 	{
 		return 0;
 	}
-	FILE *steg = fopen("sample.bmp", "rb+");
+	steg = fopen("sample.bmp", "rb+");
 	if (steg==NULL)
 	{
-		printf("The Data File Does Not Exist.\n");
-		return 1;
+		printf("The Data File Does Not Exist.\n\n");
+		goto start;
 	}
-	intrpt_header(steg);	
+	intrpt_header(steg);
+	fseek(steg, 54, SEEK_SET);	
 	if (opt==49)
 	{
-		read_data(steg);
+		printf("\n--Read Mode--\n");
+		int i, j;
+		char ch;
+		if (read_char()!=2)
+		{
+			printf("No Existing Data Found !\n\n");
+			fclose(steg);
+			goto start;
+		}
+		for(i=0;i<bitmap.height;i++)
+		{
+			for(j=0;j<bitmap.width;j++)
+			{
+				ch=read_char();
+				if (ch==10)
+				{
+					printf("\n--End Of Message--\n\n");
+					fclose(steg);
+					goto start;
+				}
+				printf("%c", ch);
+			}
+			fseek(steg, bitmap.padding, SEEK_CUR);
+		}
 	}
 	else if (opt==50)
 	{
-		write_data(steg);
+		char wr_opt,ch;
+		int i,j;
+		printf("\n--Write Mode--\nEnter 0 To OverWrite Message\nEnter 1 To Add Text To The Existing Message\n");
+		scanf("%c", &wr_opt);
+		getchar();
+		if (wr_opt==48)
+		{
+			write_pix(2);		
+		}
+		else if (wr_opt==49)
+		{
+			if (read_char()==2)
+			{
+				for(i=0;i<bitmap.height;i++)
+				{
+					for(j=0;j<bitmap.width;j++)
+					{
+						if (read_char()==10)
+						{
+							break;
+						}
+					}
+					if (j<bitmap.width)
+					{
+						break;
+					}
+					fseek(steg, bitmap.padding, SEEK_CUR);					
+				}
+			}
+			else
+			{
+				printf("No Existing Data Found. Please Start From Scratch.\n");
+				fseek(steg, -sizeof(pixel), SEEK_CUR);
+				write_pix(2);
+			}	
+		}
+		else
+		{
+			printf("Invalid Input !\n\n");
+			fclose(steg);
+			goto start;
+		}
+		fseek(steg, sizeof(pixel), SEEK_CUR);
+		i=0;	
+		do
+		{
+			fseek(steg, -sizeof(pixel), SEEK_CUR);
+			ch=getchar();
+			write_pix(ch);
+			i+=1;
+			if (i==bitmap.width)
+			{
+				fseek(steg, bitmap.padding, SEEK_CUR);
+				i=0;
+			}
+			fseek(steg, sizeof(pixel), SEEK_CUR);
+		}
+		while(ch!=10);
+		printf("\n--Message Recorded--\n\n");
+		fclose(steg);		
 	}
 	else if (opt==51)
 	{
-		reset_data(steg);
+		printf("\n--Reset Mode--\nPlease Wait");
+		int i, j;
+		fseek(steg, sizeof(pixel), SEEK_CUR);
+		for(i=0;i<bitmap.height;i++)
+		{
+			for(j=0;j<bitmap.width;j++)
+			{
+				fseek(steg, -sizeof(pixel), SEEK_CUR);
+				write_pix(3);
+				fseek(steg, sizeof(pixel), SEEK_CUR);
+			}
+			fseek(steg, bitmap.padding, SEEK_CUR);
+			if (i%8==0)
+			{
+				printf(".");
+			}
+		}
+		fclose(steg);
+		printf("\n--Bitmap Reset Successful--\n\n");
 	}
 	else 
 	{
